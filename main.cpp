9f0a58c7e02a9357cf5c4670c26ece76659780e8
TitovaVD <lera.titova.2000@list.ru>
2019-04-14 14:19:10 +0300

    code:изменён тип переменной numbers

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ main() {
     cerr << "Enter number count: ";
     cin >> number_count;
 
-    vector<double> numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(number_count);
 
     size_t bin_count;
     cerr << "Enter column count: ";
