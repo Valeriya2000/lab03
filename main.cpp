362c531050d966b886ed22a6ed38fb74ae8a1704
TitovaVD <lera.titova.2000@list.ru>
2019-04-14 18:23:35 +0300

    code:изменён цвет вывода гистрграммы

--- main.cpp
+++ main.cpp
@@ -103,9 +103,9 @@ void svg_text(double left, double baseline, string text)
 {
     cout<<"<text x='"<<left<<"' y='"<<baseline<<"'>"<<text<<"</text>";
 }
-void svg_rect(double x, double y, double width, double height)
+void svg_rect(double x, double y, double width, double height,string stroke = "black", string fill = "black")
 {
-    cout<< "<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' />";
+    cout<< "<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' height='"<<height<<"' />";
 }
 void
 show_histogram_svg(const vector<size_t>& bins) {
@@ -115,11 +115,14 @@ show_histogram_svg(const vector<size_t>& bins) {
     const auto TEXT_BASELINE = 20;
     const auto TEXT_WIDTH = 50;
     const auto BIN_HEIGHT = 30;
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
     double top = 0;
 for (size_t bin : bins) {
-    const double bin_width = 10 * bin;
+  const double bin_width = 10 * bin;
     svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+    svg_rect(0, 0, 100, 200);             // svg_rect(0, 0, 100, 200, "black", "black");
+    svg_rect(0, 0, 100, 200, "red");      // svg_rect(0, 0, 100, 200, "red",   "black");
+    svg_rect(0, 0, 100, 200, "blue", "green");
     top += BIN_HEIGHT;
 }
 svg_end();
@@ -139,5 +142,6 @@ main() {
     const auto bins = make_histogram(numbers, bin_count);
 
     show_histogram_svg(bins);
+
     return 0;
 }
