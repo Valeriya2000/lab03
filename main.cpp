26632abe51cf6086fb0a00e5cb62e4b6dd61ca70
TitovaVD <lera.titova.2000@list.ru>
2019-04-14 14:29:27 +0300

    code:выделена функция расчёта количества чисел в столбцах гистограммы и отображения гистограммы

--- main.cpp
+++ main.cpp
@@ -27,6 +27,66 @@ find_minmax(vector<double> numbers, double& min, double& max)
             max=number;
     }
 }
+vector<size_t>
+make_histogram(const vector<double>& numbers, size_t bin_count)
+{
+    double min ;
+    double max ;
+    find_minmax(numbers, min, max);
+
+        vector<size_t> bins(bin_count);
+        for (double number : numbers)
+        {
+            size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+            if (bin == bin_count)
+            {
+                bin--;
+            }
+            bins[bin]++;
+        }
+    return bins;
+}
+void show_histogram_text(vector<size_t> bins)
+{
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+
+    size_t max_count = 0;
+    for (size_t count : bins)
+    {
+        if (count > max_count)
+        {
+            max_count = count;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins)
+    {
+        if (bin < 100)
+        {
+            cout << ' ';
+        }
+        if (bin < 10)
+        {
+            cout << ' ';
+        }
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed)
+        {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+        }
+
+        for (size_t i = 0; i < height; i++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+}
 int
 main() {
     // Ввод данных
@@ -39,49 +99,8 @@ main() {
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
-    double min, max;
-    find_minmax(numbers, min, max);
-    vector<size_t> bins(bin_count);
-    for (double number : numbers) {
-        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count) {
-            bin--;
-        }
-        bins[bin]++;
-    }
+    const auto bins = make_histogram(numbers, bin_count);
 
-    // Вывод данных
-    const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
-
-    size_t max_count = 0;
-    for (size_t count : bins) {
-        if (count > max_count) {
-            max_count = count;
-        }
-    }
-    const bool scaling_needed = max_count > MAX_ASTERISK;
-
-    for (size_t bin : bins) {
-        if (bin < 100) {
-            cout << ' ';
-        }
-        if (bin < 10) {
-            cout << ' ';
-        }
-        cout << bin << "|";
-
-        size_t height = bin;
-        if (scaling_needed) {
-            const double scaling_factor = (double)MAX_ASTERISK / max_count;
-            height = (size_t)(bin * scaling_factor);
-        }
-
-        for (size_t i = 0; i < height; i++) {
-            cout << '*';
-        }
-        cout << '\n';
-    }
-
+    show_histogram_text(bins);
     return 0;
 }
